4c1abaecd7b652a109f3a1aa15c83e8ed72d6104
Kato Soma <katos.arys@gmail.com>
2026-06-03 12:49:03 +0900

    Firebase接続

--- snsmvvm/snsmvvmApp.swift
+++ snsmvvm/snsmvvmApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAnalytics
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct snsmvvmApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State private var viewModel = ViewModel()
     @State private var profileViewModel = ProfileViewModel()
     @State private var seachViewModel = SearchViewModel()
+    
     
     var body: some Scene {
         WindowGroup {
@@ -19,6 +34,12 @@ struct snsmvvmApp: App {
                 .environment(viewModel)
                 .environment(profileViewModel)
                 .environment(seachViewModel)
+                .onAppear {
+                    Analytics.logEvent(
+                        AnalyticsEventScreenView,
+                        parameters: [AnalyticsParameterScreenName: "\(Self.self)", AnalyticsParameterScreenClass: "\(Self.self)"]
+                    )
+                }
         }
     }
 }
